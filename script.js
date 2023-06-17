9f912d21da61cf6da96c086ba19b5c811ebf83d7
Tairone <taironeamaral@bol.com.br>
2023-06-17 15:04:28 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,10 @@
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+        console.log('Service Worker is registered', registration);
+      }, function(err) {
+        console.error('Registration failed:', err);
+      });
+    });
+  }
+  
