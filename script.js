4e13bb624124ddb0817d9a8a681014004f7d2d18
Tairone <taironeamaral@bol.com.br>
2023-06-18 06:30:00 -0300

    First commit base_pwa

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-      navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
-        console.log('Service Worker is registered', registration);
-      }, function(err) {
-        console.error('Registration failed:', err);
-      });
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+      console.log('Service Worker is registered', registration);
+    }, function(err) {
+      console.error('Registration failed:', err);
     });
-  }
-  
+  });
+}
--- service-worker.js
+++ service-worker.js
@@ -1,38 +1,20 @@
-var CACHE_NAME = 'V0.1';
-
 self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/style.css',
-        '/script.js',
-        '/bootstrap.min.css',
-        '/bootstrap.min.js',
-      ]);
-    })
-  );
-});
-
-self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.filter(function(cacheName) {
-          return cacheName !== CACHE_NAME;
-        }).map(function(cacheName) {
-          return caches.delete(cacheName);
-        })
-      );
-    })
+      caches.open('my-cache').then(function(cache) {
+          return cache.addAll([
+              '/',
+              '/index.html',
+              '/style.css',
+              '/script.js'
+          ]);
+      })
   );
 });
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
+      caches.match(event.request).then(function(response) {
+          return response || fetch(event.request);
+      })
   );
 });
